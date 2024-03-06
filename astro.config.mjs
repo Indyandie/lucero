38b468f5d89783a742e2881973cc74e77ace3c53
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-03-06 00:52:20 -0600

    build: add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://indyandie.github.io/',
+  base: 'lucero/',
 	integrations: [
 		starlight({
 			title: '🌕 Lucero',
