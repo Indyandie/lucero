fcba5eac0e5a247e915a7c1c98c5527f72104db3
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-10-21 23:48:44 -0500

    style: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,46 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://indyandie.github.io/',
+  site: 'https://indyandie.github.io/',
   base: '/lucero',
-	integrations: [
-		starlight({
-			title: '🌕 Lucero',
-			social: {
-				github: 'https://github.com/indyandie/lucero',
-			},
-			plugins: [
+  integrations: [
+    starlight({
+      title: '🌕 Lucero',
+      social: {
+        github: 'https://github.com/indyandie/lucero',
+      },
+      plugins: [
         // Generate the OpenAPI documentation pages.
         starlightOpenAPI([
           {
-           base: 'api/openai',
-           label: 'openAI',
-           schema: 'https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml',
+            base: 'api/openai',
+            label: 'openAI',
+            schema: 'https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml',
           },
           {
-           base: 'api/pokeapi',
-           label: 'PokeAPI',
-           schema: 'https://raw.githubusercontent.com/PokeAPI/pokeapi/master/openapi.yml',
+            base: 'api/pokeapi',
+            label: 'PokeAPI',
+            schema: 'https://raw.githubusercontent.com/PokeAPI/pokeapi/master/openapi.yml',
           },
         ]),
       ],
-			sidebar: [
-				{
-					label: 'Docs',
-					autogenerate: { directory: 'docs' },
-				},
-				{
+      sidebar: [
+        {
+          label: 'Docs',
+          autogenerate: { directory: 'docs' },
+        },
+        {
           label: 'APIs',
           items: openAPISidebarGroups,
         },
-				{
-					label: 'Elements',
-					autogenerate: { directory: 'elements' },
-				},
-			],
-		}),
-	],
-});
+        {
+          label: 'Elements',
+          autogenerate: { directory: 'elements' },
+        },
+      ],
+    }),
+  ],
+})
