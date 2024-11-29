93f7625bc57c601f10830c54474c8590d69e41c4
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-11-29 02:50:30 -0600

    feat(social): add rss link to site header

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
       },
       social: {
         github: 'https://github.com/indyandie/lucero',
+        rss: 'https://indyandie.github.io/lucero/rss',
       },
       components: {
         ContentPanel: './src/components/overrides/CustomContentPanel.astro',
