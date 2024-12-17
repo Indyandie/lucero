b526900ec858f3ad7d0565f266506640660b98d4
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-12-17 01:19:28 -0600

    chore(deps): bump - astro `5.0.9`, @astrojs/starlight `0.30.2`

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,14 @@
 import { defineCollection, z } from 'astro:content'
+import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
 
 export const collections = {
   docs: defineCollection({
+    loader: docsLoader(),
     schema: docsSchema({
       extend: z.object({
         pubDate: z.date().optional(),
-        rss: z.boolean().optional()
+        rss: z.boolean().optional(),
       }),
     }),
   }),
